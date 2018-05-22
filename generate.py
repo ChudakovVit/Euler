8cc899f28346136f0e132d8db52ae40cb6d72d6e
chudakovvit <ChudakovVit@yandex.ru>
2018-05-22 14:26:22 +0300

    N in class get/set

--- generate.py
+++ generate.py
@@ -27,29 +27,15 @@ def get_field_from_number(number, x, y):
     return x_list
 
 
-def generate_recursive_3_3(i, j):
-    side_size = FIELD.get_side_size()
-    for sub_i in range(i, side_size):
-        FIELD.change_cell(sub_i, j)
+def generate_field(x, y):
+    """
+    Сгенерируем поле заданных размеров
+    :param x: высота
+    :param y: ширина
+    :return:
+    """
+    for i in range(2**(x*y)):
+        print(i)
+        FIELD.set(get_field_from_number(i, x, y))
+        FIELD.set_side_size(x+2)
         file_output_field()
-        generate_recursive_3_3(sub_i + 1, j)
-        for sub_j in range(j, side_size):
-            FIELD.change_cell(sub_i, sub_j)
-            file_output_field()
-            generate_recursive_3_3(sub_i, sub_j+1)
-    #
-    # if i == FIELD.get_side_size():
-    #     i = 0
-    # else:
-    #     FIELD.change_cell(i, j)
-    #     print('#1 ' + str(i) + ' ' + str(j))
-    #     file_output_field()
-    #     generate_recursive_3_3(i+1, j)
-    # if j == FIELD.get_side_size():
-    #     return
-    # else:
-    #     FIELD.change_cell(i, j)
-    #     print('#2 ' + str(i) + ' ' + str(j))
-    #     file_output_field()
-    #     generate_recursive_3_3(i, j+1)
-
--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from generate import *
 
 
 def main():
-    FIELD.set(get_zero_field(N-2))
-    FIELD.set_side_size(N)
+    # FIELD.set(get_zero_field(N-2))
+    # FIELD.set_side_size(N)
     # view.set_field()
-    file_output_field()
-    generate_recursive_3_3(0, 0)
-    # generate_field()
+    # file_output_field()
+    generate_field(4, 4)
     # view.mainloop()
 
 
